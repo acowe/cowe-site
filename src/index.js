74ef911c8a074a717f663eefa5d8c0e5a0eefc8f
acowe <arcowe12@gmail.com>
2024-01-06 23:11:15 -0500

    rerouting

--- src/index.js
+++ src/index.js
@@ -4,14 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, HashRouter} from 'react-router-dom'
+import {BrowserRouter, HashRouter, RouterProvider, createHashRouter} from 'react-router-dom'
 
+const router = createHashRouter([
+    {
+        path: "/*",
+        element: <App />,
+    }
+]);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
 
